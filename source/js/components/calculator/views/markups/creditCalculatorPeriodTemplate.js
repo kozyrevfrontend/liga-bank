1487f10fafb4997bb5bf713937408cc4b8f474d5
Ilya Kozyrev <kozyrev.frontend@gmail.com>
2020-12-04 11:46:42 +0300

    добавляет tablet и mobile стили для calculator

--- source/js/components/calculator/views/markups/creditCalculatorPeriodTemplate.js
+++ source/js/components/calculator/views/markups/creditCalculatorPeriodTemplate.js
@@ -1,4 +1,24 @@
 export function createCalculatorPeriodTemplate(minimumCreditPeriod, maximumCreditPeriod) {
+  let yearsMin = `лет`;
+
+  if (minimumCreditPeriod === 1) {
+    yearsMin = `год`;
+  }
+
+  if (minimumCreditPeriod >= 2 && minimumCreditPeriod <= 4) {
+    yearsMin = `года`;
+  }
+
+  let yearMax = `лет`;
+
+  if (maximumCreditPeriod === 1) {
+    yearMax = `год`;
+  }
+
+  if (maximumCreditPeriod >= 2 && maximumCreditPeriod <= 4) {
+    yearMax = `года`;
+  }
+
   return (
     `<div class="calculator__section" id="periodSection">
       <h4 class="calculator__title-inner">Срок кредитования</h4>
@@ -6,8 +26,8 @@ export function createCalculatorPeriodTemplate(minimumCreditPeriod, maximumCredi
         <input class="calculator__range range" id="periodRange" type="range" min="${minimumCreditPeriod}" max="${maximumCreditPeriod}" step="1" value="${minimumCreditPeriod}">
       </p>
       <p class="calculator__legend">
-        <span>${minimumCreditPeriod} лет</span>
-        <span>${maximumCreditPeriod} лет</span>
+        <span>${minimumCreditPeriod} ${yearsMin}</span>
+        <span>${maximumCreditPeriod} ${yearMax}</span>
       </p>
     </div>`
   );
