313c3db2251a2c560d10df75e87ce675ca454159
Ilya Kozyrev <kozyrev.frontend@gmail.com>
2020-12-13 11:15:27 +0300

    удаляет :nth-child() из стилей

--- source/js/components/calculator/views/markups/mortgage/calculatorMortgageOrderTemplate.js
+++ source/js/components/calculator/views/markups/mortgage/calculatorMortgageOrderTemplate.js
@@ -13,26 +13,28 @@ export function createMortgageCalculatorOrderTemplate(creditSumm, downPayment, c
     `<div class="calculator__form form">
       <h3 class="form__title">Шаг 3. Оформление заявки</h3>
       <form id="calculatorForm" action="https://echo.htmlacademy.ru" method="POST">
-        <p class="form__item form__item--readonly">
-          <label for="orderNumber">Номер заявки</label>
-          <input id="orderNumber" name="orderNumber" type="text" value="№ 00${orderCount}" tabindex="-1" readonly>
-        </p>
-        <p class="form__item form__item--readonly">
-          <label for="creditPropose">Цель кредита</label>
-          <input id="creditPropose" name="creditPropose" type="text" value="Ипотека" tabindex="-1" readonly>
-        </p>
-        <p class="form__item form__item--readonly">
-          <label for="creditSumm">Стоимость недвижимости</label>
-          <input id="creditSumm" name="creditSumm" type="text" value="${creditSumm.toLocaleString(`ru-RU`)} рублей" tabindex="-1" readonly>
-        </p>
-        <p class="form__item form__item--readonly">
-          <label for="downPayment">Первоначальный взнос</label>
-          <input id="downPayment" name="downPayment" type="text" value="${downPayment.toLocaleString(`ru-RU`)} рублей" tabindex="-1" readonly>
-        </p>
-        <p class="form__item form__item--readonly">
-          <label for="creditPeriod">Срок кредитования</label>
-          <input id="creditPeriod" name="creditPeriod" type="text" value="${creditPeriod} ${years}" tabindex="-1" readonly>
-        </p>
+        <div class="form__info">
+          <p class="form__item form__item--readonly">
+            <label for="orderNumber">Номер заявки</label>
+            <input id="orderNumber" name="orderNumber" type="text" value="№ 00${orderCount}" tabindex="-1" readonly>
+          </p>
+          <p class="form__item form__item--readonly">
+            <label for="creditPropose">Цель кредита</label>
+            <input id="creditPropose" name="creditPropose" type="text" value="Ипотека" tabindex="-1" readonly>
+          </p>
+          <p class="form__item form__item--readonly">
+            <label for="creditSumm">Стоимость недвижимости</label>
+            <input id="creditSumm" name="creditSumm" type="text" value="${creditSumm.toLocaleString(`ru-RU`)} рублей" tabindex="-1" readonly>
+          </p>
+          <p class="form__item form__item--readonly">
+            <label for="downPayment">Первоначальный взнос</label>
+            <input id="downPayment" name="downPayment" type="text" value="${downPayment.toLocaleString(`ru-RU`)} рублей" tabindex="-1" readonly>
+          </p>
+          <p class="form__item form__item--readonly">
+            <label for="creditPeriod">Срок кредитования</label>
+            <input id="creditPeriod" name="creditPeriod" type="text" value="${creditPeriod} ${years}" tabindex="-1" readonly>
+          </p>
+        </div>
         <p class="form__item">
           <label class="visually-hidden" for="userName">Введите ваши ФИО</label>
           <input id="userName" name="userName" type="text" placeholder="ФИО" required autofocus>
